acabf0654ec3f496927e3b5479d3a7aa569f4204
j-gatsby <timjoeckel@gmail.com>
2014-03-19 13:25:41 -0400

    Improve comments for init-test

--- init-test.c
+++ init-test.c
@@ -3,22 +3,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int main()
-{   
-	printf("How many columns and rows: ");
-  	int d = GetInt();
-  
-  	int i = d;		// declared this in main()
-  	int j = d;		// declared this in main()
-  	int k = (d * d) -1;
+int main()														// for testing only
+{   															// for testing only
+	printf("How many columns and rows: ");						// for testing only
+  	int d = GetInt();											// for testing only
+  	int i = d;													// declare this in main()
+  	int j = d;													// declare this in main()
+  	int k = (d * d) -1;											// declare this in main()
+  	int board[i][j];											// declare this in main()
+ 	int tile[k];												// declare this in main()
   	
-  	int board[i][j];		// declared this in main()
- 	int spot[k];			
-  	board[i][j] = spot[k];
+  	board[i][j] = tile[k];
 	while (k > 0)
   	{
   		for (i = 0; i < d; i++)
 			for (j = 0; j < d; j++, k--)
-			printf("board[%i][%i] = spot[%i]\n", i, j, k);
+			printf("board[%i][%i] = tile[%i]\n", i, j, k); 		// for testing only
 	}
-}
+}																// for testing only
