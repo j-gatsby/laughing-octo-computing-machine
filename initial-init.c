3079e152c7bb553d5ff5798cb29f0c2ff9c5f841
j-gatsby <timjoeckel@gmail.com>
2014-03-19 12:25:27 -0400

    Add initial init.c to review later

---
+++ initial-init.c
@@ -0,0 +1,29 @@
+  // from fifteen.c
+  // initial code, first try, 5 or ten mins.  
+  //  spent 6 hours on it after that.
+  
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    // TODO
+  int i = d;		// declared this in main()
+  int j = d;		// declared this in main()
+  int top = d - 1;	// top number, with 0-indexing
+  board[i][j];		// declared this in main()
+  spot[k];			// 
+  for (i=0; i < top; i++)				//  number of rows on the board
+ 
+  	for (j=0; j < top; i++)  				// number of columns on the board
+    	
+    	for (k = top*top-1; k >= 0; k--)		// values for spots on the board,
+    											//	from top*top-1 to 0, 
+    											//	  which will be the space.
+    		{
+    			board[i][j] = spot[k];			
+    		}									
+}
+
