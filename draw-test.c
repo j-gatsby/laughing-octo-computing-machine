e426b99903f5e6ddd575f286ba5e554e369ce6a9
j-gatsby <timjoeckel@gmail.com>
2014-03-22 04:06:51 -0400

    Swap tile 2 and 1, if totals number of tiles are odd

--- draw-test.c
+++ draw-test.c
@@ -9,20 +9,44 @@ int main()														// for testing only
   	int d = GetInt();											// for testing only
   	int i = d;													
   	int j = d;													
-  	int tile = (d * d) -1;										
-  	int board[i][j];							
- 											
-	while (tile > 0)
-  	{
-  		for (i = 0; i < d; i++)
-  		{
-			for (j = 0; j < d; j++, tile--)
+  	int tile = (d * d) - 1;	
+  	int top = (d * d) - 1;									
+  	int board[i][j];
+  	int onefortwo = 1;
+  	int twoforone = 2;							
+ 	bool odd;
+ 	if (top % 2 == 1)
+ 	{
+ 		odd = true;
+ 	}
+ 	else
+ 	{
+ 		odd = false;
+ 	}
+ 		
+	for (i = 0; i < d; i++)
+	{
+		for (j = 0; j < d; j++, tile--)
+		{
+			if (odd == true && tile == 2)
+			{
+				board[i][j] = onefortwo;
+				printf("board[%i][%i] = %i\n", i, j, tile);			// for testing only
+			}
+			else if (odd == true && tile == 1)
+			{
+				board[i][j] = twoforone;
+				printf("board[%i][%i] = %i\n", i, j, tile);			// for testing only
+			}
+			else
 			{
 				board[i][j] = tile;
 				printf("board[%i][%i] = %i\n", i, j, tile); 		// for testing only
 			}
 		}
 	}
+	// possibly set the winning conditions here, by:
+	// another for loop-for loop-count tiles back up, with special conditions if tile == 0  ??????
 																
 	int rows;
     int columns;
@@ -33,7 +57,14 @@ int main()														// for testing only
     	printf("\n\n\n\n");
     	for (columns = 0; columns < d; columns++, tile--)
     	{
-    		printf("    %2i    ", tile);
+    		if (tile == 0)
+    		{
+    			printf("    __    ");
+    		}
+    		else
+    		{
+    			printf("    %2i    ", tile);
+    		}
     	}
     }
     printf("\n\n\n\n");
