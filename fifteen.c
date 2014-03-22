cf1e381cb41b90829d4d660a7c8b32aea007d139
j-gatsby <timjoeckel@gmail.com>
2014-03-22 06:28:52 -0400

    Transfer code from test file to fifteen.c

--- fifteen.c
+++ fifteen.c
@@ -133,20 +133,50 @@ void greet(void)
 void init(void)
 {
     // TODO
-    int i = d;								
+//	 int d = GetInt();								// for testing only
+  	int i = d;													
   	int j = d;													
-  	int k = (d * d) -1;											
-  	int board[i][j];											
- 	int tile[k];
-    
-  	board[i][j] = tile[k];
-	while (k > 0)
-  	{
-  		for (i = 0; i < d; i++)
-			for (j = 0; j < d; j++, k--);
-	}						
-	
+  	int tile = (d * d) - 1;	
+  	int top = (d * d) - 1;									
+  	int board[i][j];
+  	int onefortwo = 1;
+  	int twoforone = 2;							
+ 	bool odd;
+ 	
+ 	if (top % 2 == 1)
+ 	{
+ 		odd = true;
+ 	}
+ 	else
+ 	{
+ 		odd = false;
+ 	}
+ 		
+	for (i = 0; i < d; i++)
+	{
+		for (j = 0; j < d; j++, tile--)
+		{
+			if (odd == true && tile == 2)
+			{
+				board[i][j] = onefortwo;
+			//	printf("board[%d][%d] = %d\n", i, j, board[i][j]);	// for testing only
+			}
+			else if (odd == true && tile == 1)
+			{
+				board[i][j] = twoforone;
+			//	printf("board[%d][%d] = %d\n", i, j, board[i][j]);	// for testing only
+			}
+			else
+			{
+				board[i][j] = tile;
+			//	printf("board[%d][%d] = %d\n", i, j, board[i][j]); 		// for testing only
+			}
+		}
+	}
 }
+	// possibly set the winning conditions here, by:
+	// another for loop-for loop-count tiles back up, with special conditions if tile == 0  ??????
+										
 
 /**
  * Prints the board in its current state.
@@ -154,7 +184,30 @@ void init(void)
 void draw(void)
 {
     // TODO
+   //int rows;
+    //int columns;
+    //tile = (d * d) -1;
+    
+    //for (rows = 0; rows < d; rows++)
+    for (i = 0; i < d; i++)
+    {
+    	printf("\n\n\n");
+    	//for (columns = 0; columns < d; columns++, tile--)
+    	for (j = 0; j < d; j++)
+    	{
+    		if (board[i][j] == 0)
+    		{
+    			printf("   __   ");
+    		}
+    		else
+    		{
+    			printf("   %2d   ", board[i][j]);
+    		}
+    	}
+    }
+    printf("\n\n\n\n");  
 }
+
 
 /**
  * If tile borders empty space, moves tile and returns true, else
